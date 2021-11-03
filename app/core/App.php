05acb60c7c8025490f6844f19760c80c8b2cd681
indahkiranad <kiranadamayanti10@gmail.com>
2021-11-03 11:16:32 +0800

    Test

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,23 @@
 
 class App{
     
+<<<<<<< HEAD
+  public function __construct() {
+    $url = $this->parseURL();
+    var_dump($url);
+    }
+
+    public function parseURL() {
+    if(isset($_GET['url'])) {
+      $url = rtrim($_GET['url'],"/");
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);      
+    return $url;
+    }
+  }
+=======
     public function __construct() {
         echo "Web MVC Siap Dijalankan!";
       }
+>>>>>>> ad74d43ae813facf27745845ce3aeea83b06926f
 }
